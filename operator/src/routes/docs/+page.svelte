5c811f0d1ce49fad967590343bcd296ff15595e7
Siva Ragavan <siva@ragavan.com>
2025-09-07 22:29:21 +0000

    chore: bump version to 0.6.9

--- operator/src/routes/docs/+page.svelte
+++ operator/src/routes/docs/+page.svelte
@@ -4,7 +4,7 @@
   import Card from '/src/components/bootstrap/Card.svelte';
 
   // Hard-coded docs version; update during version bumps
-  const API_VERSION = '0.6.8 (v0)';
+  const API_VERSION = '0.6.9 (v0)';
   
 
   setPageTitle('API Documentation');
