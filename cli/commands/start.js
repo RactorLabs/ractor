bbe770e3cf9e902469892056bb34292593c52a2e
Siva Ragavan <siva@ragavan.com>
2025-09-09 09:55:54 -0400

    fix(cli/start): always recreate controller container to apply updated env (ensures agents receive RAWORC_HOST_* via controller)

--- cli/commands/start.js
+++ cli/commands/start.js
@@ -402,12 +402,10 @@ module.exports = (program) => {
 
             case 'controller': {
               console.log(chalk.blue('[INFO] ') + 'Ensuring controller service is running...');
-              if (await containerRunning('raworc_controller')) { console.log(chalk.green('[SUCCESS] ') + 'Controller already running'); console.log(); break; }
+              // Always recreate to apply updated environment
               if (await containerExists('raworc_controller')) {
-                await docker(['start','raworc_controller']);
-                console.log(chalk.green('[SUCCESS] ') + 'Controller started');
-                console.log();
-                break;
+                console.log(chalk.blue('[INFO] ') + 'Recreating controller to apply configuration...');
+                try { await docker(['rm','-f','raworc_controller']); } catch (_) {}
               }
               // Default OLLAMA_HOST: prefer internal container if running
               let OLLAMA_HOST = options.controllerOllamaHost || process.env.OLLAMA_HOST;
