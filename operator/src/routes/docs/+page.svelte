18585087ffd8061d19a8f36c7967cdcef1a5b901
Siva Ragavan <siva@ragavan.com>
2025-09-16 21:21:32 +0000

    chore: bump version to 0.8.0

--- operator/src/routes/docs/+page.svelte
+++ operator/src/routes/docs/+page.svelte
@@ -6,7 +6,7 @@
   import { getHostName } from '$lib/branding.js';
 
   // Hard-coded docs version; update during version bumps
-  const API_VERSION = '0.7.8 (v0)';
+  const API_VERSION = '0.8.0 (v0)';
   
 
   setPageTitle('API Documentation');
