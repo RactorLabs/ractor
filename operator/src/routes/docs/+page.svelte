0a92d891f735add2926241a60f20a13f3a2489a1
Siva Ragavan <siva@ragavan.com>
2025-09-21 03:31:20 +0000

    chore: bump version to 0.9.10

--- operator/src/routes/docs/+page.svelte
+++ operator/src/routes/docs/+page.svelte
@@ -6,7 +6,7 @@
   import { getHostName } from '$lib/branding.js';
 
   // Hard-coded docs version; update during version bumps
-  const API_VERSION = '0.9.9 (v0)';
+  const API_VERSION = '0.9.10 (v0)';
   const schemas = getCommonSchemas();
   // Compute docs once reactively and order Published section last
   $: docs = (() => {
